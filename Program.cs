09e11f7c5f0cc526c897d790c97a83cffa15c0fb
Martin Grihangne <martin.g@vendorpanel.com>
2023-02-21 15:20:15 +1100

    Initial source

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Lndscaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = "./Test files";
+            var lndFilter = "*.lnd";
+            Console.WriteLine($"Currently in {Directory.GetCurrentDirectory()}");
+            var lndFiles = Directory.EnumerateFiles(dir, lndFilter);
+            foreach (var lndFileName in lndFiles)
+            {
+                Console.WriteLine(lndFileName);
+            }
+        }
+    }
+}
